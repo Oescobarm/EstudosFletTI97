e3201256c4077e7562250a8c1516e6cd7bfd4e30
Mateus Silva Escobar <mateusescobar0@gmail.com>
2024-07-02 16:05:38 -0300

    atualizado
    atualizado

--- telas/tela1.py
+++ telas/tela1.py
@@ -19,3 +19,18 @@ def main(page:ft.Page):
 
 ft.app(main)
 
+# import flet as ft
+#
+# # modifiquei o meu code
+# #vou usar essa função para criar o layout da pagina
+# def main(page:ft.Page):
+#     page.title="Primeira pagina"
+#
+#     #Toda vez que eu alterar a minha pagina eu devo dar um update
+#     page.update()
+#
+#
+#
+# ft.app(target=main)
+
+
